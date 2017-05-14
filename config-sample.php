ba2c3612a7395f528005e989046bc017a8f91b08
Michael <Use Website Contact Please>
2017-05-13 23:57:58 -0500

    fixed config-sample

--- config-sample.php
+++ config-sample.php
@@ -1,7 +1,7 @@
 <?php
 
 //absolute path to directory housing your md files (md should be lowercase in file names)
-define('MD_BASE_PATH', '/Users/mredig/Developer/GitHub/Notes-to-Self');
+define('MD_BASE_PATH', '.');
 
 //URL path to where the css file are located for the md repo (optional)
 define('HTML_CSS_URL', 'css/styles.css');
